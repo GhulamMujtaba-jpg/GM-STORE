3b3ec0a56d817d4a06ecf1c88f306296b2534fa2
GhulamMujtaba-jpg <mujtaba6065@gmail.com>
2026-04-01 02:17:31 +0500

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-function shopNow() {
-  alert("Welcome to GM Store! Start shopping now 🛒");
-}
-
-function addToCart(productName) {
-  const msg = document.getElementById("cart-message");
-  msg.innerText = productName + " has been added to cart 🛒";
-}
